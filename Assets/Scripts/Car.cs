1a09815612afa5efd982936b7d3280e0dbb8fc57
Ignacio Inostroza <iinostroza1@miuandes.cl>
2024-03-17 21:27:56 -0300

    deacceleration

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
     public float maxSpeed = 20f;
     public float acceleration = 5f;
+    public float deceleration = 2f; // Added deceleration rate
     public float turnSpeed = 100f;
 
     private Rigidbody rb;
@@ -18,9 +20,18 @@ public class CarController : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Acceleration and braking
+        // Acceleration
         float accelerationInput = Input.GetAxis("Vertical");
-        currentSpeed = Mathf.Clamp(currentSpeed + accelerationInput * acceleration * Time.fixedDeltaTime, -maxSpeed, maxSpeed);
+        if (accelerationInput != 0f)
+        {
+            currentSpeed = Mathf.Clamp(currentSpeed + accelerationInput * acceleration * Time.fixedDeltaTime, -maxSpeed, maxSpeed);
+        }
+        else
+        {
+            // Deceleration when not accelerating
+            float decelerationAmount = Mathf.Sign(currentSpeed) * deceleration * Time.fixedDeltaTime;
+            currentSpeed = Mathf.Clamp(currentSpeed - decelerationAmount, -maxSpeed, maxSpeed);
+        }
         rb.velocity = transform.forward * currentSpeed;
 
         // Steering
